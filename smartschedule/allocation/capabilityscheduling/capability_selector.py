f1e9e83d708ca26586569d40a5bd1164152cb048
Sebastian Buczyński <nnplaya@gmail.com>
2024-09-02 21:30:41 +0200

    implemented capability selector

--- smartschedule/allocation/capabilityscheduling/capability_selector.py
+++ smartschedule/allocation/capabilityscheduling/capability_selector.py
@@ -1,18 +1,36 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+from enum import StrEnum, auto
+
 from smartschedule.shared.capability.capability import Capability
 
 
+class SelectingPolicy(StrEnum):
+    ALL_SIMULTANEOUSLY = auto()
+    ONE_OF_ALL = auto()
+
+
+@dataclass(frozen=True)
 class CapabilitySelector:
-    @staticmethod
-    def can_perform_one_of(capabilities: set[Capability]) -> CapabilitySelector:
-        return CapabilitySelector()
+    capabilities: set[Capability]
+    selecting_policy: SelectingPolicy
 
     @staticmethod
     def can_perform_all_at_the_time(
-        being_an_admin: set[Capability],
+        capabilities: set[Capability],
     ) -> CapabilitySelector:
-        return CapabilitySelector()
+        return CapabilitySelector(capabilities, SelectingPolicy.ALL_SIMULTANEOUSLY)
+
+    @staticmethod
+    def can_perform_one_of(capabilities: set[Capability]) -> CapabilitySelector:
+        return CapabilitySelector(capabilities, SelectingPolicy.ONE_OF_ALL)
 
     def can_perform(self, *capabilities: Capability) -> bool:
-        return False
+        if len(capabilities) == 1:
+            return capabilities[0] in self.capabilities
+
+        return (
+            self.selecting_policy == SelectingPolicy.ALL_SIMULTANEOUSLY
+            and self.capabilities.issuperset(set(capabilities))
+        )
